9d12890c7fffb33489c3444bf9efcc5d0d6c5755
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-24 16:47:06 +0100

    Aggiunto listato all'header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,21 +1,76 @@
 <script>
 export default {
   name: 'AppHeader',
+  data() {
+    return{
+        menuLinks: [
+                    {
+                        linkName: 'CHARACTERS',
+                    },
+                    {
+                        linkName: 'COMICS',
+                    },
+                    {
+                        linkName: 'MOVIES',
+                    },
+                    {
+                        linkName: 'TV',
+                    },
+                    {
+                        linkName: 'GAMES',
+                    },
+                    {
+                        linkName: 'COLLECTIBLES',
+                    },
+                    {
+                        linkName: 'VIDEOS',
+                    },
+                    {
+                        linkName: 'FANS',
+                    },
+                    {
+                        linkName: 'NEWS',
+                    },
+                    {
+                        linkName: 'SHOP',
+                    }
+                ]
+    }
+    
+  }
 }
 </script>
 
 <template>
-    <div class="container">
+    <header class="container">
         <div class="logo">
             <img src="../assets/dc-logo.png" alt="logo">
         </div>
-        <div class="list">
-            
-        </div>
-    </div>
+        <nav class="header__nav">
+            <ul>
+                <li v-for="link in menuLinks"><a href="#">{{link.linkName}}</a></li>
+            </ul>
+        </nav>
+    </header>
   
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    .header{
+        display: flex;
+        justify-content: space-between;
+            & img{
+                max-width: 4.375rem;
+                margin: .9375rem 0;
+            }
+            & ul{
+                height: 100%;
+                display: flex;
+                align-items: center;
+                list-style: none;
+            }
+        }
+
+
 
 </style>
