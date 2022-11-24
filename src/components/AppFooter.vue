b41037108cd691f551a1bef507b44507869bb477
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-24 21:13:13 +0100

    Aggiunto dc-bar

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,38 +1,77 @@
 <script>
 export default {
   name: 'AppFooter',
+  data(){
+    return{
+        cards: [
+            {
+                img: 'buy-comics-digital-comics.png',
+                cardName: 'DIGITAL COMICS',
+            },
+            {
+                img: 'buy-comics-merchandise.png',
+                cardName: 'DC MERCHANDISE',
+            },
+            {
+                img: 'buy-comics-subscriptions.png',
+                cardName: 'SUBSCRIPTION',
+            },
+            {
+                img: 'buy-comics-shop-locator.png',
+                cardName: 'COMIC SHOP LOCATOR',
+            },
+            {
+                img: 'buy-dc-power-visa.svg',
+                cardName: 'DC POWER VISA',
+            },
+        ]
+    }
+  }
 }
 </script>
 
 <template>
   <footer>
-    <div class="top">
-        <div class="container">
-            <img src="../assets/buy-comics-digital-comics.png" alt="digital comics">
-            <img src="../assets/buy-comics-merchandise.png" alt="merchandise">
-            <img src="../assets/buy-comics-subscriptions.png" alt="subscriptions">
-            <img src="../assets/buy-comics-shop-locator.png" alt="shoplocator">
-            <img src="../assets/buy-dc-power-visa.svg" alt="visa">
+    <div class="dc-bar">
+        <div class="cards container">
+            <div class="card" v-for="card in cards">
+                <a href="#" class="card-info">
+                    <img :src="`src/assets/${card.img}`" alt="digital.comics-img">
+                    <p>{{card.cardName}}</p>
+                </a>
+            </div>
         </div>
-
     </div>
-    <div class="center"></div>
-    <div class="under"></div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
-    .top{
-        height: 150px;
+    .dc-bar{
         background-color: var(--primary-color);
-            & .container{
+        padding: 40px 0;
+        & .cards{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            & .card:nth-child(4) img{
+                width: 40px;
+            }
+            & .card-info{
                 display: flex;
-                justify-content: space-around;
-                padding-top: 40px;
+                justify-content: center;
+                align-items: center;
+                color: white;
+                text-decoration: none;
+                font-size: 15px;
+    
+                & img{
+                    width: 40px;
+                    margin-right: 8px;
+                    max-height: 40px;
+                    
+                }
             }
-            & .container img{
-                max-width: 60px;
-                max-height: 60px;
-            }
+        }
     }
+    
 </style>
